df9b25af9a322debaf349c4040c9e3277b22e5a1
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-03-25 20:37:57 -0300

    implementa home com request a api

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../../services/app";
+import { Link } from "react-router-dom";
+
+import "./style.css";
 
 function Home() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get("/r-api/?api=filmes");
+      // console.log(response.data);
+      setFilmes(response.data);
+    }
+    loadFilmes();
+  }, []);
   return (
-    <div className="App">
-      <h1>Home</h1>
+    <div className="container--home">
+      <div className="list-movies">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <h3>{filme.nome}</h3>
+              <img src={filme.foto} alt={filme.id} />
+              <Link to="/">Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
