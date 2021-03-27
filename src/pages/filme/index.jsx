f5255d52b845da2a4c1373303aeb4262e3c1b690
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-03-26 21:04:33 -0300

    customiza e conserta bug history.replace

--- src/pages/filme/index.jsx
+++ src/pages/filme/index.jsx
@@ -14,8 +14,8 @@ export default function Filme() {
     async function loadFilmes() {
       const response = await api.get(`/r-api/?api=filmes/${id}`);
 
-      if (!response.data.length) {
-        // tentou acessar um Id que não exite
+      if (response.data.length === 0) {
+        // tentou acessar um Id que não exite , retorna pra home
         history.replace("/");
         return;
       }
@@ -23,14 +23,29 @@ export default function Filme() {
       setloading(false);
     }
     loadFilmes();
+
+    // a funcao return do useEffect  é executaada quando o componente é demontado
+    // return () => {
+    //   console.log("demontando componente");
+    // };
   }, [id, history]);
 
   if (!loading) {
     return (
-      <div className="detalhes">
+      <div className="movie-info">
         <h3>{filme.nome}</h3>
         <img src={filme.foto} alt={filme.nome} />
         <p>{filme.sinopse}</p>
+
+        <div className="botoes">
+          <button onClick={() => {}}>Salvar</button>
+          <a
+            target="blank"
+            href={`https://www.youtube.com/results?search_query=${filme.nome} Trailer`}
+          >
+            Trailer
+          </a>
+        </div>
       </div>
     );
   } else {
